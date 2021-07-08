7ac518400ebf109d04fc3ddc0289e3d880bcdec7
Jonathan Scholz <contact@jonathan-scholz.de>
2021-07-08 12:21:50 +0200

    feat(encryption): add the option for additional authenticated data

--- lib/src/encryption/handshake.dart
+++ lib/src/encryption/handshake.dart
@@ -6,8 +6,11 @@ import 'package:endguard/src/protos/protocol.pb.dart';
 
 import 'aes.dart';
 
+/// The SHA256 hash algorithm that is used by the InitialPackageEncryption.
 final sha = Sha256();
 
+/// An EncryptedPackage contains the ciphertext of a package
+/// and the key used to encrypt it.
 class EncryptedPackage {
   final Uint8List _ciphertext;
   final SecretKey _key;
@@ -16,42 +19,51 @@ class EncryptedPackage {
       : _ciphertext = ciphertext,
         _key = key;
 
+  /// Returns the ciphertext of the package.
   Uint8List exportPackage() {
     return _ciphertext;
   }
 
+  /// Returns the key that was used to encrypt the package.
   SecretKey exportKey() {
     return _key;
   }
 }
 
+/// The InitialPackageEncryption is used to encrypt the packages
+/// for the handshake.
 class InitialPackageEncryption {
-  Future<SecretKey> _generate256BitKey(Uint8List plaintext) async {
+  /// Generates the SHA256 hash of the [plaintext].
+  Future<SecretKey> _hashSHA256(Uint8List plaintext) async {
     final hash = await sha.hash(plaintext);
     final key = SecretKeyData(hash.bytes);
     return key;
   }
 
+  /// Validates the [plaintext] and the [key] match.
+  /// The [key] must be the exact SHA256 hash of the [plaintext].
   Future<void> _validatePackage(Uint8List plaintext, SecretKey key) async {
-    final checksum = await _generate256BitKey(plaintext);
+    final checksum = await _hashSHA256(plaintext);
     if (checksum != key) {
       throw InvalidHandshakePackageException(
           'validation failed: wrong key/checksum');
     }
   }
 
+  /// Encrypts a [package] using its SHA256 hash as key.
   Future<EncryptedPackage> encryptPackage(Uint8List package) async {
-    final key = await _generate256BitKey(package);
-    final e = await AES.aesCbcEncrypt(package, key);
+    final key = await _hashSHA256(package);
+    final e = await AES.encryptAES256_GCM(package, key);
     final bytes = e.writeToBuffer();
 
     return EncryptedPackage(bytes, key: key);
   }
 
+  /// Decrypts an encrypted [package] using the [key].
   Future<Uint8List> decryptPackage(Uint8List package, {SecretKey key}) async {
     final e = EncryptedMessage.fromBuffer(package);
 
-    final bytes = await AES.aesCbcDecrypt(e, key);
+    final bytes = await AES.decryptAES256_GCM(e, key);
     await _validatePackage(bytes, key);
     return bytes;
   }
